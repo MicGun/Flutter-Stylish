a53718cb4f3863ad8b3990303454754d2faf04a7
Hugh Wang <hugh@easychat.co>
2023-05-08 22:27:58 +0800

    success to call by peerId

--- stylish/lib/video_call/video_call_screen.dart
+++ stylish/lib/video_call/video_call_screen.dart
@@ -25,18 +25,71 @@ class _VideoCallScreenState extends State<VideoCallScreen> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    _connect(context);
+    initRenderers();
+    _initServer(context);
   }
 
-  void _connect(BuildContext context) async {
-    _callingServer ??= VideoCallingServer(context);
+  initRenderers() async {
+    await _localRenderer.initialize();
+    await _remoteRenderer.initialize();
+  }
+
+  void _initServer(BuildContext context) async {
+    _callingServer ??= VideoCallingServer(context)..connect();
 
     _selfId = await _callingServer?.getSelfId();
-    if(_selfId != null) {
-      setState(() {
-        
-      });
+    if (_selfId != null) {
+      setState(() {});
     }
+
+    _callingServer?.onCallStateChange =
+        (Session session, CallState state) async {
+      switch (state) {
+        case CallState.CallStateNew:
+          setState(() {
+            _session = session;
+          });
+          break;
+        case CallState.CallStateRinging:
+          bool? accept = await _showAcceptDialog(session.pid);
+          if (accept!) {
+            _accept();
+            setState(() {
+              _inCalling = true;
+            });
+          } else {
+            _reject();
+          }
+          break;
+        case CallState.CallStateBye:
+          if (_waitAccept) {
+            print('peer reject');
+            _waitAccept = false;
+            Navigator.of(context).pop(false);
+          }
+          setState(() {
+            _localRenderer.srcObject = null;
+            _remoteRenderer.srcObject = null;
+            _inCalling = false;
+            _session = null;
+          });
+          break;
+        case CallState.CallStateInvite:
+          _waitAccept = true;
+          _showInvateDialog(session.pid);
+          break;
+        case CallState.CallStateConnected:
+          if (_waitAccept) {
+            _waitAccept = false;
+            Navigator.of(context).pop(false);
+          }
+          setState(() {
+            _inCalling = true;
+          });
+
+          break;
+      }
+    };
 
     _callingServer?.onLocalStream = ((stream) {
       _localRenderer.srcObject = stream;
@@ -51,23 +104,166 @@ class _VideoCallScreenState extends State<VideoCallScreen> {
     _callingServer?.onRemoveRemoteStream = ((_, stream) {
       _remoteRenderer.srcObject = null;
     });
+
+    // _callingServer?.onLocalStream = ((stream) {
+    //   _localRenderer.srcObject = stream;
+    //   setState(() {});
+    // });
+
+    // _callingServer?.onAddRemoteStream = ((_, stream) {
+    //   _remoteRenderer.srcObject = stream;
+    //   _inCalling = true;
+    //   setState(() {});
+    // });
+
+    // _callingServer?.onRemoveRemoteStream = ((_, stream) {
+    //   // _remoteRenderer.srcObject = null;
+    //   setState(() {
+    //     _inCalling = false;
+    //     _waitAccept = false;
+    //     _localRenderer.srcObject = null;
+    //     _remoteRenderer.srcObject = null;
+    //     _inCalling = false;
+    //     _session = null;
+    //   });
+    // });
+  }
+
+  _invitePeer(BuildContext context, String peerId, bool useScreen) async {
+    if (_callingServer != null && peerId != _selfId) {
+      _callingServer?.invite(peerId, 'video', useScreen);
+    }
+  }
+
+  _accept() {
+    if (_session != null) {
+      _callingServer?.accept(_session!.sid, 'video');
+    }
+  }
+
+  _reject() {
+    if (_session != null) {
+      _callingServer?.reject(_session!.sid);
+    }
+  }
+
+  _hangUp() {
+    if (_session != null) {
+      _callingServer?.bye(_session!.sid);
+    }
+  }
+
+  _muteMic() {
+    _callingServer?.muteMic();
+  }
+
+  // void _createVideoCall() async {
+  //   if (_peerId != null && _peerId!.isNotEmpty) {
+  //     _session = await _callingServer?.createCall(_peerId!);
+  //     if (_session != null) {
+  //       setState(() {
+  //         _waitAccept = true;
+  //       });
+  //     }
+  //   }
+  // }
+
+  // void _JoinVideoCall() async {
+  //   if (_peerId != null && _peerId!.isNotEmpty) {
+  //     _session = await _callingServer?.joinCall(_peerId!);
+  //     if (_session != null) {
+  //       setState(() {
+  //         _waitAccept = true;
+  //       });
+  //     }
+  //   }
+  // }
+
+  // _hangUp() {
+  //   if (_session != null) {
+  //     _callingServer?.leaveCall(_session!.sid);
+  //   }
+  // }
+
+  Future<bool?> _showAcceptDialog(String peerId) {
+    return showDialog<bool?>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Calling..."),
+          content: Text('你收到來自 $peerId 的來電'),
+          actions: <Widget>[
+            MaterialButton(
+              child: Text(
+                '拒接',
+                style: TextStyle(color: Color(0xff575a69)),
+              ),
+              onPressed: () => Navigator.of(context).pop(false),
+            ),
+            MaterialButton(
+              child: Text(
+                '接起',
+                style: TextStyle(color: Colors.blueAccent),
+              ),
+              onPressed: () => Navigator.of(context).pop(true),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<bool?> _showInvateDialog(String peerId) {
+    return showDialog<bool?>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Calling"),
+          content: Text("正在等待 $peerId 接通電話"),
+          actions: <Widget>[
+            TextButton(
+              child: Text("取消"),
+              onPressed: () {
+                Navigator.of(context).pop(false);
+                _hangUp();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:  const Text('Video Call'),
+        title: const Text('Video Call'),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: _inCalling
-          ? FloatingActionButton(
-              child: const Icon(
-                Icons.call_end,
-                color: Colors.red,
+          ? Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              FloatingActionButton(
+                child: const Icon(
+                  Icons.call_end,
+                  color: Colors.red,
+                ),
+                onPressed: () {
+                  _hangUp();
+                },
               ),
-              onPressed: () {},
-            )
+              FloatingActionButton(
+                child: const Icon(
+                  Icons.mic_off_rounded,
+                  color: Colors.blueAccent,
+                ),
+                onPressed: () {
+                  _muteMic();
+                },
+              ),]
+          )
           : null,
       body: _inCalling
           ? OrientationBuilder(builder: (context, orientation) {
@@ -97,28 +293,29 @@ class _VideoCallScreenState extends State<VideoCallScreen> {
               ]);
             })
           : IntrinsicWidth(
-            child: Column(
+              child: Column(
                 children: [
                   Row(
                     children: [
                       Padding(
                         padding: EdgeInsets.only(top: 16, left: 16, right: 16),
                         child: Text(
-                          'Your Id : ${_selfId}',
-                          style: TextStyle(
+                          'Your Id : $_selfId',
+                          style: const TextStyle(
                             fontSize: 24,
                           ),
-                          ),),
+                        ),
+                      ),
                     ],
                   ),
                   Row(
                     children: [
-                      SizedBox(
-                        width: 350,
+                      Expanded(
                         child: Padding(
                             padding: const EdgeInsets.only(
                                 top: 16, left: 16, right: 16),
                             child: TextField(
+                              keyboardType: TextInputType.number,
                               // textInputAction: TextInputAction.search,
                               // onSubmitted: (value) {
                               //   getSearchLocation(value);
@@ -144,39 +341,83 @@ class _VideoCallScreenState extends State<VideoCallScreen> {
                   ),
                   Row(
                     children: [
-                      Padding(
-                        padding: EdgeInsets.all(16),
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Color(0x00000000),
-                            minimumSize: const Size(300, 60),
-                            shape: const RoundedRectangleBorder(
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(5),
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.all(16),
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.pinkAccent,
+                              minimumSize: const Size(300, 60),
+                              shape: const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(5),
+                                ),
                               ),
                             ),
-                          ),
-                          onPressed: () {
-                            //to call peer
-                          },
-                          child: const Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Text(
-                              '打給他',
-                              style: TextStyle(
-                                color: Color(0xffffffff),
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
+                            onPressed: () {
+                              //to call peer
+                              if (_peerId != null && _peerId!.isNotEmpty) {
+                                _invitePeer(context, _peerId!, false);
+                              }
+                              // _createVideoCall();
+                            },
+                            child: const Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Text(
+                                '打給他',
+                                style: TextStyle(
+                                  color: Color(0xffffffff),
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
                             ),
                           ),
                         ),
                       ),
+                      // Expanded(
+                      //   child: Padding(
+                      //     padding: EdgeInsets.all(16),
+                      //     child: ElevatedButton(
+                      //       style: ElevatedButton.styleFrom(
+                      //         backgroundColor: Colors.purpleAccent,
+                      //         minimumSize: const Size(300, 60),
+                      //         shape: const RoundedRectangleBorder(
+                      //           borderRadius: BorderRadius.all(
+                      //             Radius.circular(5),
+                      //           ),
+                      //         ),
+                      //       ),
+                      //       onPressed: () {
+                      //         //to call peer
+                      //         _JoinVideoCall();
+                      //       },
+                      //       child: const Padding(
+                      //         padding: EdgeInsets.all(8.0),
+                      //         child: Text(
+                      //           '加入他',
+                      //           style: TextStyle(
+                      //             color: Color(0xffffffff),
+                      //             fontSize: 18,
+                      //             fontWeight: FontWeight.bold,
+                      //           ),
+                      //         ),
+                      //       ),
+                      //     ),
+                      //   ),
+                      // ),
                     ],
                   )
                 ],
               ),
-          ),
+            ),
     );
+  }
+
+  @override
+  void dispose() {
+    _localRenderer.dispose();
+    _remoteRenderer.dispose();
+    super.dispose();
   }
 }
--- stylish/lib/video_call/video_call_server.dart
+++ stylish/lib/video_call/video_call_server.dart
@@ -1,7 +1,13 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 import 'package:flutter/widgets.dart';
 import 'package:flutter_webrtc/flutter_webrtc.dart';
 import 'package:random_string/random_string.dart';
+import 'package:stylish/video_call/turn.dart';
+import 'package:stylish/video_call/websocket.dart';
+
+import 'device_info.dart';
 
 class Session {
   Session({required this.sid, required this.pid});
@@ -12,16 +18,39 @@ class Session {
   List<RTCIceCandidate> remoteCandidates = [];
 }
 
+enum SignalingState {
+  ConnectionOpen,
+  ConnectionClosed,
+  ConnectionError,
+}
+
+enum CallState {
+  CallStateNew,
+  CallStateRinging,
+  CallStateInvite,
+  CallStateConnected,
+  CallStateBye,
+}
+
 class VideoCallingServer {
   VideoCallingServer(
     this._context,
   );
+
+  JsonEncoder _encoder = JsonEncoder();
+  JsonDecoder _decoder = JsonDecoder();
+  
   final String _host = 'demo.cloudwebrtc.com';
+  var _port = 8086;
+  var _turnCredential;
   String _selfId = randomNumeric(6);
   BuildContext? _context;
   MediaStream? _localStream;
   List<MediaStream> _remoteStreams = <MediaStream>[];
   List<RTCRtpSender> _senders = <RTCRtpSender>[];
+  Map<String, Session> _sessions = {};
+  SimpleWebSocket? _socket;
+  // VideoSource _videoSource = VideoSource.Camera;
 
   Function(MediaStream stream)? onLocalStream;
   Function(Session session, RTCDataChannel dc, RTCDataChannelMessage data)?
@@ -29,7 +58,9 @@ class VideoCallingServer {
   Function(Session session, RTCDataChannel dc)? onDataChannel;
   Function(Session session, MediaStream stream)? onAddRemoteStream;
   Function(Session session, MediaStream stream)? onRemoveRemoteStream;
+  Function(SignalingState state)? onSignalingStateChange;
   Function(dynamic event)? onPeersUpdate;
+  Function(Session session, CallState state)? onCallStateChange;
 
   String get sdpSemantics => 'unified-plan';
 
@@ -56,6 +87,19 @@ class VideoCallingServer {
 
   Future<String> getSelfId() async {
     return _selfId;
+  }
+
+  Future<void> _closeSession(Session session) async {
+    _localStream?.getTracks().forEach((element) async {
+      await element.stop();
+    });
+    await _localStream?.dispose();
+    _localStream = null;
+
+    await session.pc?.close();
+    await session.dc?.close();
+    _senders.clear();
+    // _videoSource = VideoSource.Camera;
   }
 
   Future<Session> _createSession(
@@ -104,18 +148,18 @@ class VideoCallingServer {
       // This delay is needed to allow enough time to try an ICE candidate
       // before skipping to the next one. 1 second is just an heuristic value
       // and should be thoroughly tested in your own environment.
-      // await Future.delayed(
-      //     const Duration(seconds: 1),
-      //     () => _send('candidate', {
-      //           'to': peerId,
-      //           'from': _selfId,
-      //           'candidate': {
-      //             'sdpMLineIndex': candidate.sdpMLineIndex,
-      //             'sdpMid': candidate.sdpMid,
-      //             'candidate': candidate.candidate,
-      //           },
-      //           'session_id': sessionId,
-      //         }));
+      await Future.delayed(
+          const Duration(seconds: 1),
+          () => _send('candidate', {
+                'to': peerId,
+                'from': _selfId,
+                'candidate': {
+                  'sdpMLineIndex': candidate.sdpMLineIndex,
+                  'sdpMid': candidate.sdpMid,
+                  'candidate': candidate.candidate,
+                },
+                'session_id': sessionId,
+              }));
     };
 
     pc.onIceConnectionState = (state) {};
@@ -133,6 +177,303 @@ class VideoCallingServer {
 
     newSession.pc = pc;
     return newSession;
+  }
+
+  Future<void> connect() async {
+    var url = 'https://$_host:$_port/ws';
+    _socket = SimpleWebSocket(url);
+
+    print('connect to $url');
+
+    if (_turnCredential == null) {
+      try {
+        _turnCredential = await getTurnCredential(_host, _port);
+        /*{
+            "username": "1584195784:mbzrxpgjys",
+            "password": "isyl6FF6nqMTB9/ig5MrMRUXqZg",
+            "ttl": 86400,
+            "uris": ["turn:127.0.0.1:19302?transport=udp"]
+          }
+        */
+        _iceServers = {
+          'iceServers': [
+            {
+              'urls': _turnCredential['uris'][0],
+              'username': _turnCredential['username'],
+              'credential': _turnCredential['password']
+            },
+          ]
+        };
+      } catch (e) {}
+    }
+
+    _socket?.onOpen = () {
+      print('onOpen');
+      onSignalingStateChange?.call(SignalingState.ConnectionOpen);
+      _send('new', {
+        'name': DeviceInfo.label,
+        'id': _selfId,
+        'user_agent': DeviceInfo.userAgent
+      });
+    };
+
+    _socket?.onMessage = (message) {
+      print('Received data: ' + message);
+      onMessage(_decoder.convert(message));
+    };
+
+    _socket?.onClose = (int? code, String? reason) {
+      print('Closed by server [$code => $reason]!');
+      onSignalingStateChange?.call(SignalingState.ConnectionClosed);
+    };
+
+    await _socket?.connect();
+  }
+
+  _send(event, data) {
+    var request = Map();
+    request["type"] = event;
+    request["data"] = data;
+    _socket?.send(_encoder.convert(request));
+  }
+
+  void onMessage(message) async {
+    Map<String, dynamic> mapData = message;
+    var data = mapData['data'];
+
+    switch (mapData['type']) {
+      case 'peers':
+        {
+          List<dynamic> peers = data;
+          if (onPeersUpdate != null) {
+            Map<String, dynamic> event = Map<String, dynamic>();
+            event['self'] = _selfId;
+            event['peers'] = peers;
+            onPeersUpdate?.call(event);
+          }
+        }
+        break;
+      case 'offer':
+        {
+          var peerId = data['from'];
+          var description = data['description'];
+          var media = data['media'];
+          var sessionId = data['session_id'];
+          var session = _sessions[sessionId];
+          var newSession = await _createSession(session,
+              peerId: peerId,
+              sessionId: sessionId,
+              media: media,
+              screenSharing: false);
+          _sessions[sessionId] = newSession;
+          await newSession.pc?.setRemoteDescription(
+              RTCSessionDescription(description['sdp'], description['type']));
+          // await _createAnswer(newSession, media);
+
+          if (newSession.remoteCandidates.length > 0) {
+            newSession.remoteCandidates.forEach((candidate) async {
+              await newSession.pc?.addCandidate(candidate);
+            });
+            newSession.remoteCandidates.clear();
+          }
+          onCallStateChange?.call(newSession, CallState.CallStateNew);
+          onCallStateChange?.call(newSession, CallState.CallStateRinging);
+        }
+        break;
+      case 'answer':
+        {
+          var description = data['description'];
+          var sessionId = data['session_id'];
+          var session = _sessions[sessionId];
+          session?.pc?.setRemoteDescription(
+              RTCSessionDescription(description['sdp'], description['type']));
+          onCallStateChange?.call(session!, CallState.CallStateConnected);
+        }
+        break;
+      case 'candidate':
+        {
+          var peerId = data['from'];
+          var candidateMap = data['candidate'];
+          var sessionId = data['session_id'];
+          var session = _sessions[sessionId];
+          RTCIceCandidate candidate = RTCIceCandidate(candidateMap['candidate'],
+              candidateMap['sdpMid'], candidateMap['sdpMLineIndex']);
+
+          if (session != null) {
+            if (session.pc != null) {
+              await session.pc?.addCandidate(candidate);
+            } else {
+              session.remoteCandidates.add(candidate);
+            }
+          } else {
+            _sessions[sessionId] = Session(pid: peerId, sid: sessionId)
+              ..remoteCandidates.add(candidate);
+          }
+        }
+        break;
+      case 'leave':
+        {
+          var peerId = data as String;
+          _closeSessionByPeerId(peerId);
+        }
+        break;
+      case 'bye':
+        {
+          var sessionId = data['session_id'];
+          print('bye: ' + sessionId);
+          var session = _sessions.remove(sessionId);
+          if (session != null) {
+            onCallStateChange?.call(session, CallState.CallStateBye);
+            _closeSession(session);
+          }
+        }
+        break;
+      case 'keepalive':
+        {
+          print('keepalive response!');
+        }
+        break;
+      default:
+        break;
+    }
+  }
+
+  void _closeSessionByPeerId(String peerId) {
+    var session;
+    _sessions.removeWhere((String key, Session sess) {
+      var ids = key.split('-');
+      session = sess;
+      return peerId == ids[0] || peerId == ids[1];
+    });
+    if (session != null) {
+      _closeSession(session);
+      onCallStateChange?.call(session, CallState.CallStateBye);
+    }
+  }
+
+  Future<Session> createCall(String peerId) async {
+    // var sessionId = (int.parse(_selfId) > int.parse(peerId))? '$_selfId-$peerId' : '$peerId-$_selfId';
+    var sessionId = '$_selfId-$peerId';
+    Session session = await _createSession(null,
+        peerId: peerId,
+        sessionId: sessionId,
+        media: 'video',
+        screenSharing: false);
+    RTCSessionDescription s = await session.pc!.createOffer({});
+    await session.pc!.setLocalDescription(_fixSdp(s));
+    _sessions[sessionId] = session;
+    return session;
+  }
+
+  void invite(String peerId, String media, bool useScreen) async {
+    var sessionId = _selfId + '-' + peerId;
+    Session session = await _createSession(null,
+        peerId: peerId,
+        sessionId: sessionId,
+        media: media,
+        screenSharing: useScreen);
+    _sessions[sessionId] = session;
+    // if (media == 'data') {
+    //   _createDataChannel(session);
+    // }
+    _createOffer(session, media);
+    onCallStateChange?.call(session, CallState.CallStateNew);
+    onCallStateChange?.call(session, CallState.CallStateInvite);
+  }
+
+  Future<void> _createOffer(Session session, String media) async {
+    try {
+      RTCSessionDescription s =
+          await session.pc!.createOffer({});
+      await session.pc!.setLocalDescription(_fixSdp(s));
+      _send('offer', {
+        'to': session.pid,
+        'from': _selfId,
+        'description': {'sdp': s.sdp, 'type': s.type},
+        'session_id': session.sid,
+        'media': media,
+      });
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  void accept(String sessionId, String media) {
+    var session = _sessions[sessionId];
+    if (session == null) {
+      return;
+    }
+    _createAnswer(session, media);
+  }
+
+  Future<void> _createAnswer(Session session, String media) async {
+    try {
+      RTCSessionDescription s =
+          await session.pc!.createAnswer({});
+      await session.pc!.setLocalDescription(_fixSdp(s));
+      _send('answer', {
+        'to': session.pid,
+        'from': _selfId,
+        'description': {'sdp': s.sdp, 'type': s.type},
+        'session_id': session.sid,
+      });
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  void reject(String sessionId) {
+    var session = _sessions[sessionId];
+    if (session == null) {
+      return;
+    }
+    bye(session.sid);
+  }
+
+  void bye(String sessionId) {
+    _send('bye', {
+      'session_id': sessionId,
+      'from': _selfId,
+    });
+    var sess = _sessions[sessionId];
+    if (sess != null) {
+      _closeSession(sess);
+    }
+  }
+
+  void muteMic() {
+    if (_localStream != null) {
+      bool enabled = _localStream!.getAudioTracks()[0].enabled;
+      _localStream!.getAudioTracks()[0].enabled = !enabled;
+    }
+  }
+
+  // Future<Session> joinCall(String peerId) async {
+  //   var sessionId = '$peerId-$_selfId';
+  //   Session session = await _createSession(null,
+  //       peerId: peerId,
+  //       sessionId: sessionId,
+  //       media: 'video',
+  //       screenSharing: false);
+  //   RTCSessionDescription s = await session.pc!.createOffer({});
+  //   await session.pc!.setLocalDescription(_fixSdp(s));
+  //   _sessions[sessionId] = session;
+  //   return session;
+  // }
+
+  // void leaveCall(String sessionId) {
+  //   // var sess = _sessions[sessionId];
+  //   var session = _sessions.remove(sessionId);
+  //   if (session != null) {
+  //     _closeSession(session);
+  //   }
+  // }
+
+  RTCSessionDescription _fixSdp(RTCSessionDescription s) {
+    var sdp = s.sdp;
+    s.sdp =
+        sdp!.replaceAll('profile-level-id=640c1f', 'profile-level-id=42e032');
+    return s;
   }
 
   void _addDataChannel(Session session, RTCDataChannel channel) {
